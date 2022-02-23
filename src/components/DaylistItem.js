8c55818ea6dc860c72591b5b617d6a6449fb4747
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-02-23 08:34:43 -0800

    DayList and DayListItems working

---
+++ src/components/DaylistItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  console.log(props);
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  });
+
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    }
+    if (props.spots === 1) {
+      return '1 spot remaining'
+    }
+    return props.spots + ' spots remaining';
+  }
+  
+  return (
+    <li className={dayClass} onClick={props.setDay}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
