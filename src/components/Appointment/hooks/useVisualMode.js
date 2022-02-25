bb49b24502fe0e4a574538e8b2135d6c9e7098a6
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-02-25 12:08:24 -0800

    back function in hook

--- src/components/Appointment/hooks/useVisualMode.js
+++ src/components/Appointment/hooks/useVisualMode.js
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode) {
-    setMode(newMode);
+  const transition = function(newMode, replace = false) {
+    if (newMode !== mode) {
+      setMode(newMode);
+      (replace
+        ? history[history.length - 1] = newMode
+        : history.push(newMode)
+      );
+      setHistory([ ...history ]);
+    }
   }
 
-  return { mode, transition };
+  const back = function() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+      setHistory([ ...history ]);
+    }
+  }
+
+  return { mode, transition, back };
 }
